587c64009f24e1794a3e709fc87d36cc11853bab
h004888 <huybeoku@gmail.com>
2026-07-21 22:02:16 +0700

    big update

--- pcms-common/src/main/java/com/pcms/common/config/FeignClientConfig.java
+++ pcms-common/src/main/java/com/pcms/common/config/FeignClientConfig.java
@@ -2,10 +2,12 @@ package com.pcms.common.config;
 
 import com.pcms.common.feign.FeignErrorDecoder;
 import feign.codec.ErrorDecoder;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ConditionalOnClass(ErrorDecoder.class)
 public class FeignClientConfig {
 
     @Bean
